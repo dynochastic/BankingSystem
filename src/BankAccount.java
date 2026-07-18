45f71dcc954310813f179f3ece45e8f22b12c757
Christian Adam Rendon <christianrendon077@gmail.com>
2026-07-18 18:40:02 +0800

    Transaction class added

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,12 +1,12 @@
 public class BankAccount {
-     int AccountNumber;
-     double Balance;
-     int CustomerNumber;
+     private int AccountNumber;
+     private double Balance;
+     private int CustomerNumber;
 
     BankAccount(int AccountNumber, double Balance, int CustomerNumber){
         this.AccountNumber = AccountNumber;
         this.Balance = Balance;
         this.CustomerNumber = CustomerNumber;
-
     }
+
 }
---
+++ src/Transaction.java
@@ -0,0 +1,13 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Transaction {
+    private int trasactionID;
+    private LocalDateTime transactDate;
+    private int AccountNumber;
+
+    Transaction(int transactionID, LocalDateTime transactDate, int AccountNumber){
+
+
+    }
+}
