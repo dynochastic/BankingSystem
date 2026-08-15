cc7f2278f7b064f8f660d90c3658539aee52d415
Christian Adam Rendon <christianrendon077@gmail.com>
2026-08-16 07:37:37 +0800

    Added connect method

--- src/repository/CustomerRepository.java
+++ src/repository/CustomerRepository.java
@@ -1,9 +1,23 @@
 package repository;
 
 import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class CustomerRepository{
 
-    String CONNECTION = "DATABASEHERE";
+    String CONNECTION = "eqwewq";
+
+
+    void Connect(){
+        try{
+            Connection connection = DriverManager.getConnection(CONNECTION);
+            System.out.println("Connection Connected Successfully");
+        } catch(SQLException sqlEx){
+            System.out.println("Failed");
+            sqlEx.printStackTrace();
+        }
+    }
 
 }
