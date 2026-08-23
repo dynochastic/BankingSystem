54ea0b484939b2ba4d964440b4ecc447a9c1760b
Christian Adam Rendon <christianrendon077@gmail.com>
2026-08-23 08:07:39 +0800

    Added postgre config

---
+++ src/config/DatabaseConnection.java
@@ -0,0 +1,21 @@
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    String CONNECTION = "eqwewq";
+
+    //This method will be used for connecting database to the program
+    void Connect(){
+        try{
+            Connection connection = DriverManager.getConnection(CONNECTION);
+            System.out.println("Connection Connected Successfully");
+        } catch(SQLException sqlEx){
+            System.out.println("Failed");
+            sqlEx.printStackTrace();
+        }
+    }
+}
--- src/repository/CustomerRepository.java
+++ src/repository/CustomerRepository.java
@@ -6,19 +6,5 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class CustomerRepository{
-git
-    String CONNECTION = "eqwewq";
-
-
-    //This method will be used for connecting database to the program
-    void Connect(){
-        try{
-            Connection connection = DriverManager.getConnection(CONNECTION);
-            System.out.println("Connection Connected Successfully");
-        } catch(SQLException sqlEx){
-            System.out.println("Failed");
-            sqlEx.printStackTrace();
-        }
-    }
 
 }
