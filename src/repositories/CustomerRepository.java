b60fa17dac0000e6c21554622fb0fa8d6b822b65
Christian Adam Rendon <christianrendon077@gmail.com>
2026-08-25 18:08:05 +0800

    Added add create method for customer

--- src/repositories/CustomerRepository.java
+++ src/repositories/CustomerRepository.java
@@ -1,7 +1,38 @@
 package repositories;
 
+import config.DatabaseConnection;
 import models.Customer;
 
-public class CustomerRepository{
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+public class CustomerRepository {
+
+    public final DatabaseConnection connectDB;
+
+    CustomerRepository(DatabaseConnection connectDB) {
+        this.connectDB = connectDB;
+
+    }
+
+    public void createUser(Customer customer) {
+        String query = "INSERT INTO customers(first_name, middle_name, last_name, birth_date, sex) values (?,?,?,?,?)";
+
+
+        try(Connection connection = connectDB.connect();
+            PreparedStatement statement = connection.prepareStatement(query);
+        ){
+            statement.setString(1,customer.getFirstName());
+            statement.setString(2, customer.getMiddleName());
+            statement.setString(3, customer.getLastName());
+            statement.setDate(4, Date.valueOf(customer.getBirthDate()));
+            statement.setString(5, String.valueOf(customer.getSex()));
+        }
+        catch (SQLException e){
+            System.out.println("Failed to save customer");
+            e.printStackTrace();
+        }
+    }
 }
