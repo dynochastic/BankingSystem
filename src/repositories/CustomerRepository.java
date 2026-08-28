2044f3cab8297c7927fffe086a19daa7a091177d
Christian Adam Rendon <christianrendon077@gmail.com>
2026-08-28 23:08:36 +0800

    Added customer ID retreival from database to the program

--- src/repositories/CustomerRepository.java
+++ src/repositories/CustomerRepository.java
@@ -3,10 +3,8 @@ package repositories;
 import config.DatabaseConnection;
 import models.Customer;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.lang.annotation.Retention;
+import java.sql.*;
 
 public class CustomerRepository {
 
@@ -19,12 +17,10 @@ public class CustomerRepository {
 
     public void createCustomer(Customer customer) {
         String query = "INSERT INTO customers(first_name, middle_name, last_name, birth_date, sex) values (?,?,?,?,?)";
-
-
+        String addAddressQuery = "INSERT INTO address(brgy, municipality, province, postal_code, country) values (?, ?, ?, ?, ?) where customer_ID = ?";
         try {
             Connection connection = connectDB.connect();
             PreparedStatement statement = connection.prepareStatement(query);
-
             statement.setString(1,customer.getFirstName());
             statement.setString(2, customer.getMiddleName());
             statement.setString(3, customer.getLastName());
@@ -34,11 +30,30 @@ public class CustomerRepository {
             statement.executeUpdate();
 
             statement.close();
+
+            try{
+                PreparedStatement addressStatement = connection.prepareStatement(addAddressQuery);
+                statement.setString(1, customer.getAddress().getBrgy());
+                statement.setString(2, customer.getAddress().getMunicipality());
+                statement.setString(3, customer.getAddress().getProvince());
+                statement.setString(4, customer.getAddress().getCountry());
+                statement.setString(5, customer.getAddress().getPostal());
+
+                //To check if the retrieval is correct
+                statement.setLong(6, customer.getCustomerID());
+
+
+            }catch (SQLException e){
+                System.out.println("Failed to add Address");
+            }
             connection.close();
+
+
         }
         catch (SQLException e){
             System.out.println("Failed to save customer");
             e.printStackTrace();
         }
+
     }
 }
