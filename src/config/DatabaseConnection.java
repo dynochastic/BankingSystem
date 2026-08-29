e10e77223c060682e12aeaac285d4e95cda108a7
Christian Adam Rendon <christianrendon077@gmail.com>
2026-08-29 13:34:44 +0800

    Refactor the connection exception

--- src/config/DatabaseConnection.java
+++ src/config/DatabaseConnection.java
@@ -6,19 +6,18 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    private String CONNECTION = "jdbc:postgresql://localhost:5432/bank_management_db";
-    private String username = "postgres";
-    private String password = "passwordHere";
-    //This  will be used for connecting database to the program
-    public Connection connect() {
-        try {
-            DriverManager.getConnection(CONNECTION);
-            System.out.println("Connection Connected Successfully");
-        } catch (SQLException sqlEx) {
-            System.out.println("Failed");
-            sqlEx.printStackTrace();
 
-        }
-        return null;
+    //This  will be used for connecting database to the program
+    public Connection connect() throws SQLException{
+
+        String CONNECTION = System.getenv("CONNECTION");
+        String username = System.getenv("DB_USERNAME");
+        String password = System.getenv("DB_PASSWORD");
+
+        Connection connection =DriverManager.getConnection(CONNECTION,username,password);
+
+        System.out.println("Connection Connected Successfully");
+
+        return connection;
     }
 }
