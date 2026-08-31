74303dfaec80016708eb4a31c1af270f41d8eb1f
Christian Adam Rendon <christianrendon077@gmail.com>
2026-08-31 22:16:44 +0800

    Disabled autocommit and restructured create operations for customer

--- src/repositories/CustomerRepository.java
+++ src/repositories/CustomerRepository.java
@@ -3,7 +3,6 @@ package repositories;
 import config.DatabaseConnection;
 import models.Customer;
 
-import java.lang.annotation.Retention;
 import java.sql.*;
 
 public class CustomerRepository {
@@ -12,69 +11,67 @@ public class CustomerRepository {
 
     public CustomerRepository() {
         this.connectDB = new DatabaseConnection();
-
-    }
-    long getID(){
-        return Statement.RETURN_GENERATED_KEYS;
     }
 
-    public void createCustomer(Customer customer) {
-        String query = "INSERT INTO customers(first_name, middle_name, last_name, birth_date, sex) values (?,?,?,?,?) RETURNING customer_id";
-        String query2 = "INSERT INTO address(brgy, municipality, province, postal_code, country) values (?, ?, ?, ?, ?) where customer_id = ?";
-        String query3 = "INSERT INTO contacts(mobile_number, email_address, telephone_number) values (?,?,?) WHERE customer_id = ?";
+    public void createCustomer(Customer customer) throws SQLException {
+
+        Connection connection = connectDB.connect();
+        String query = "INSERT INTO customers(first_name, middle_name, last_name, birth_date, sex) values (?,?,?,?,?)";
+        String query2 = "INSERT INTO address(brgy, municipality, province, country, postal_code, customer_id) values (?, ?, ?, ?, ?, ?)";
+        String query3 = "INSERT INTO contacts(mobile_number, email_address, telephone_number, customer_id) values (?,?,?,?)";
+        long customer_id = 0;
+
+        connection.setAutoCommit(false);
 
 
+        try (PreparedStatement statement = connection.prepareStatement(query);
+             PreparedStatement addressStatement = connection.prepareStatement(query2);
+             PreparedStatement contactStatement = connection.prepareStatement(query3);) {
 
-        try {
-            Connection connection = connectDB.connect();
-            PreparedStatement statement = connection.prepareStatement(query);
-            statement.setString(1,customer.getFirstName());
+            statement.setString(1, customer.getFirstName());
             statement.setString(2, customer.getMiddleName());
             statement.setString(3, customer.getLastName());
             statement.setDate(4, Date.valueOf(customer.getBirthDate()));
             statement.setString(5, String.valueOf(customer.getSex()));
 
-            statement.executeUpdate();
-
-            statement.close();
-
-            try{
-                PreparedStatement addressStatement = connection.prepareStatement(query2, Statement.RETURN_GENERATED_KEYS);
-                addressStatement.setString(1, customer.getAddress().getBrgy());
-                addressStatement.setString(2, customer.getAddress().getMunicipality());
-                addressStatement.setString(3, customer.getAddress().getProvince());
-                addressStatement.setString(4, customer.getAddress().getCountry());
-                addressStatement.setString(5, customer.getAddress().getPostal());
-
-                //To check if the retrieval is correct
-                addressStatement.setLong(6, getID());
-
-                addressStatement.executeUpdate();
-                addressStatement.close();
-                try{
-                    PreparedStatement contactStatement = connection.prepareStatement(query3);
-                    contactStatement.setString(1, customer.getContact().getPhoneNumber() );
-                    contactStatement.setString(2, customer.getContact().getEmailAddress());
-                    contactStatement.setString(3, customer.getContact().getTelephoneNumber());
-                    contactStatement.setLong(4, getID());
-
-                    contactStatement.executeUpdate();
-                    contactStatement.close();
+            try (ResultSet rs = statement.executeQuery()){
+                if (!rs.next()) {
+                    throw new SQLException("ID cannot be mapped.");
                 }
-                catch (SQLException e){
-                    System.out.println("failed to add contact details");
-
-                }
-
-            }catch (SQLException e){
-                System.out.println("Failed to add Address");
+                customer_id = rs.getLong("customer_id");
             }
-            connection.close();
+
+            addressStatement.setString(1, customer.getAddress().getBrgy());
+            addressStatement.setString(2, customer.getAddress().getMunicipality());
+            addressStatement.setString(3, customer.getAddress().getProvince());
+            addressStatement.setString(4, customer.getAddress().getCountry());
+            addressStatement.setString(5, customer.getAddress().getPostal());
+            addressStatement.setLong(6, customer_id);
+
+            addressStatement.executeUpdate();
+
+            contactStatement.setString(1, customer.getContact().getPhoneNumber());
+            contactStatement.setString(2, customer.getContact().getEmailAddress());
+            contactStatement.setString(3, customer.getContact().getTelephoneNumber());
+            contactStatement.setLong(4, customer_id);
+
+            contactStatement.executeUpdate();
+
+            connection.commit();
+            System.out.println("Customer Added to the database.");
         }
         catch (SQLException e){
-            System.out.println("Failed to save customer");
-            e.printStackTrace();
+            try {
+                connection.rollback();
+            }
+             catch (SQLException rollbackException){
+                rollbackException.printStackTrace();
+            }
+            System.out.println("Repository failed. Customer not saved");
+            throw e;
         }
-
+        finally {
+            connection.close();
+        }
     }
 }
