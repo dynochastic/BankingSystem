83ddc5c417d55090a07368418e806998583fad88
Christian Adam Rendon <christianrendon077@gmail.com>
2026-09-09 07:22:31 +0800

    queries

---
+++ src/resources/queries.sql
@@ -0,0 +1,49 @@
+-- =========================================
+-- CUSTOMER QUERIES
+-- =========================================
+
+-- Get all customers with address and contacts
+SELECT
+    c.customer_id,
+    c.first_name,
+    c.middle_name,
+    c.last_name,
+    c.birth_date,
+    c.sex,
+    a.brgy,
+    a.municipality,
+    a.province,
+    a.postal_code,
+    a.country,
+    ct.mobile_number,
+    ct.email_address,
+    ct.telephone_number
+FROM customers c
+         JOIN address a
+              ON c.customer_id = a.customer_id
+         JOIN contacts ct
+              ON c.customer_id = ct.customer_id;
+
+
+-- Get one customer
+SELECT
+    c.customer_id,
+    c.first_name,
+    c.middle_name,
+    c.last_name,
+    c.birth_date,
+    c.sex,
+    a.brgy,
+    a.municipality,
+    a.province,
+    a.postal_code,
+    a.country,
+    ct.mobile_number,
+    ct.email_address,
+    ct.telephone_number
+FROM customers c
+         JOIN address a
+              ON c.customer_id = a.customer_id
+         JOIN contacts ct
+              ON c.customer_id = ct.customer_id
+WHERE c.customer_id = 1;
