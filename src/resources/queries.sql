4820c30fb2efa0ad77e96eafbe1ad8725a95965a
Christian Adam Rendon <christianrendon077@gmail.com>
2026-09-11 22:40:45 +0800

    updated queries

--- src/resources/queries.sql
+++ src/resources/queries.sql
@@ -47,3 +47,53 @@ FROM customers c
          LEFT JOIN contacts ct
               ON c.customer_id = ct.customer_id
 WHERE c.customer_id = 1;
+
+-- =========================================
+-- CUSTOMER QUERIES
+-- =========================================
+
+-- Get all customers with address and contacts
+SELECT
+    c.customer_id,
+    c.first_name,
+    c.middle_name,
+    c.last_name,
+    c.birth_date,
+    c.sex,
+    a.brgy,
+    a.municipality,
+    a.province,
+    a.postal_code,
+    a.country,
+    ct.mobile_number,
+    ct.email_address,
+    ct.telephone_number
+FROM customers c
+         LEFT JOIN address a
+                   ON c.customer_id = a.customer_id
+         LEFT JOIN contacts ct
+                   ON c.customer_id = ct.customer_id;
+
+
+-- Get one customer
+SELECT
+    c.customer_id,
+    c.first_name,
+    c.middle_name,
+    c.last_name,
+    c.birth_date,
+    c.sex,
+    a.brgy,
+    a.municipality,
+    a.province,
+    a.postal_code,
+    a.country,
+    ct.mobile_number,
+    ct.email_address,
+    ct.telephone_number
+FROM customers c
+         LEFT JOIN address a
+                   ON c.customer_id = a.customer_id
+         LEFT JOIN contacts ct
+                   ON c.customer_id = ct.customer_id
+WHERE c.customer_id = 1;
